7af444577f8ba9e6f0eba37def57b46ac43ec319
Matt Carroll <matthew-carroll@users.noreply.github.com>
2024-06-13 02:02:34 -0700

    Initial commit - barebones generating website with a single experimental shader page

---
+++ bin/flutter_shaders_com.dart
@@ -0,0 +1,23 @@
+import 'package:static_shock/static_shock.dart';
+
+Future<void> main(List<String> arguments) async {
+  // Configure the static website generator.
+  final staticShock = StaticShock()
+    // Here, you can directly hook into the StaticShock pipeline. For example,
+    // you can copy an "images" directory from the source set to build set:
+    ..pick(DirectoryPicker.parse("images"))
+    ..pick(ExtensionPicker("mp4"))
+    // All 3rd party behavior is added through plugins, even the behavior
+    // shipped with Static Shock.
+    ..plugin(const MarkdownPlugin())
+    ..plugin(const JinjaPlugin())
+    ..plugin(const PrettyUrlsPlugin())
+    ..plugin(const SassPlugin())
+    ..plugin(const TailwindPlugin(
+      input: "source/styles/tailwind.css",
+      output: "build/styles/tailwind.css",
+    ));
+
+  // Generate the static website.
+  await staticShock.generateSite();
+}
